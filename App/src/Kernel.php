2ff423c232d21162b16e11b2c43526bab92cf6f9
Jerome Gx <jerome@gangneux.net>
2023-02-19 11:26:04 +0100

    boilerplate code

---
+++ App/src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jrmgx\Etl;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
