5eca7ae4ff41ea6fdb67858aa9ecfb30bc4395aa
Jerome Gx <jerome@gangneux.net>
2023-02-19 16:54:06 +0100

    Simple transform update

--- App/README.md
+++ App/README.md
@@ -1,1 +1,8 @@
 composer dump-autoload --classmap-authoritative
+
+TODO
+cleanup code
+implement mapping and expression language
+json in => csv out
+use https://symfony.com/doc/current/components/options_resolver.html instead of config stuff
+https://symfony.com/doc/current/components/expression_language.html
--- App/src/Transform/SimpleTransform.php
+++ App/src/Transform/SimpleTransform.php
@@ -4,17 +4,47 @@ namespace Jrmgx\Etl\Transform;
 
 use Jrmgx\Etl\Config\MappingConfig;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
 #[AsTaggedItem(index: 'simple')]
 class SimpleTransform implements TransformInterface
 {
+    private PropertyAccessorInterface $propertyAccessor;
+
+    public function __construct()
+    {
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessorBuilder()
+            ->enableExceptionOnInvalidIndex()
+            ->getPropertyAccessor();
+    }
+
     /**
-     * @param array<mixed> $data
+     * @param array<mixed> $lines
      * @return array<mixed>
      */
-    public function map(array $data, MappingConfig $config): array
+    public function map(array $lines, MappingConfig $config): array
     {
-        // TODO handle expression + expression language
-        return $data;
+        $mapping = $config->getMapping();
+        if (count($mapping) === 0) {
+            return $lines; // identity
+        }
+
+        $keys = array_map(fn (string $k) => substr($k, 3), array_keys($mapping));
+        $values = array_map(fn (string $v) => substr($v, 4), array_values($mapping));
+        $mapping = array_combine($keys, $values);
+
+        $result = [];
+
+        foreach ($lines as $line) {
+            $newLine = [];
+            foreach ($mapping as $mappingIn => $mappingOut) {
+                $obj = json_decode(json_encode($line)); // TODO could be better
+                $newLine[$mappingOut] = $this->propertyAccessor->getValue($obj, $mappingIn);
+            }
+            $result[] = $newLine;
+        }
+
+        return $result;
     }
 }
